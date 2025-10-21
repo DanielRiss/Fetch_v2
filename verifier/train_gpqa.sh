e77aaad077e531c35d3d03cb19bfd1066909e096
wind-o <t.p.d.zeinstra@student.tue.nl>
2025-10-21 19:53:09 +0200

    update train_gpqa.sh

--- verifier/train_gpqa.sh
+++ verifier/train_gpqa.sh
@@ -1,4 +1,14 @@
-# Todo: set paths
+#!/bin/bash
+#SBATCH --partition=gpu_a100
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=32
+#SBATCH --nodes=1
+#SBATCH --job-name=Train_GPQA_verifier
+#SBATCH --time=03:00:00
+# Redirect SLURM’s own logs into the job-specific folder
+#SBATCH --output=~/projects/Fetch/search/beamsearch/slurm_output_%j.txt
+#SBATCH --error=~/projects/Fetch/search/beamsearch/slurm_error_%j.txt
+
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 MODEL=meta-llama/Llama-3.1-8B-Instruct
 TRAIN_DATA="$SCRIPT_DIR/../gpqa/dataset/gpqa_training_split.csv"
